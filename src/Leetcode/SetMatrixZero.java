f718ccd927b519bc01322f281c40cde95c616070
piyush <sab9861@gmail.com>
2024-10-06 22:58:49 +0530

    set matrix zero..

--- src/Leetcode/SetMatrixZero.java
+++ src/Leetcode/SetMatrixZero.java
@@ -9,29 +9,41 @@ public class SetMatrixZero {
 		// TODO Auto-generated method stub
 		int matrix[][]= {{1,1,1},{1,0,1},{1,1,1}};
 		
-		 List<Integer>li=new ArrayList<>();
-
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    li.add(i);
-                    li.add(j);
-                }
-            }
-        }
-
-        for(int i=0;i<li.size();i=i+2){
-            int temp=li.get(i);
-            int temp2=li.get(i+1);
-            for(int j=0;j<matrix.length;j++){
-                for(int k=0;k<matrix[0].length;k++) {
-                	if(j==temp || temp2==k) {
-                		matrix[j][k]=0;
-                	}
-                }
-            }
-        }
-		System.out.println(li);
+//		List<Integer>li=new ArrayList<>();
+//
+//        for(int i=0;i<matrix.length;i++){
+//            for(int j=0;j<matrix[0].length;j++){
+//                if(matrix[i][j]==0){
+//                    for(int x=0;x<matrix.length;x++) {
+//                    	matrix[i][0]=0;
+//                    }
+////                    for(int x=i;x>=0;x--) {
+////                    	matrix[i][0]=0;
+////                    }
+//                    for(int x=0;x<matrix[0].length;x++) {
+//                    	matrix[0][j]=0;
+//                    }
+////                    for(int x=j;x>=0;x--) {
+////                    	matrix[0][j]=0;
+////                    }
+//                }
+//            }
+//        }
+//
+////        for(int i=0;i<li.size();i=i+2){
+////            int temp=li.get(i);
+////            int temp2=li.get(i+1);
+////            for(int j=0;j<matrix.length;j++){
+////                for(int k=0;k<matrix[0].length;k++) {
+////                	if(j==temp || temp2==k) {
+////                		matrix[j][k]=0;
+////                	}
+////                }
+////            }
+////        }
+////		System.out.println(li);
+////	
+		optimize(matrix);
 		
 		for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
@@ -39,6 +51,50 @@ public class SetMatrixZero {
             }System.out.println();
         }
 		
+	}
+	
+	static void optimize(int matrix[][]) {
+		boolean rowZero=false;
+		boolean colZero=false;
+		
+		for(int i=0;i<matrix.length;i++) {
+			if(matrix[i][0]==0) {
+				rowZero=true;
+			}
+		}
+		for(int i=0;i<matrix[0].length;i++) {
+			if(matrix[0][i]==0) {
+				colZero=true;
+			}
+		}
+		
+		for(int i=0;i<matrix.length;i++) {
+			for(int j=0;j<matrix[0].length;j++) {
+				if(matrix[i][j]==0) {
+					matrix[i][0]=0;
+					matrix[0][j]=0;
+				}
+			}
+		}
+		
+		for(int i=1;i<matrix.length;i++) {
+			for(int j=1;j<matrix[0].length;j++) {
+				if(matrix[i][0]==0||matrix[0][j]==0) {
+					matrix[i][j]=0;
+				}
+			}
+		}
+		
+		if(rowZero) {
+			for(int i=0;i<matrix.length;i++) {
+				matrix[i][0]=0;
+			}
+		}
+		if(colZero) {
+			for(int i=0;i<matrix[0].length;i++) {
+				matrix[0][i]=0;
+			}
+		}
 	}
 
 }
