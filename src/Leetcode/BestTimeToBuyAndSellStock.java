3b0f9a2c056fd2f841b5554f3922abbfc90a4779
Piyushh-hash <thakurpiyush011@gmail.com>
2024-10-11 23:26:45 +0530

    ..

--- src/Leetcode/BestTimeToBuyAndSellStock.java
+++ src/Leetcode/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,19 @@
 package Leetcode;
 
 public class BestTimeToBuyAndSellStock {
-
+	public static void main(String args[]) {
+		int arr[]= {7,1,5,3,6,4};
+		
+		int min=Integer.MAX_VALUE;
+		int profit=0;
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]<min) {
+				min=arr[i];
+			}else if(arr[i]-min>profit) {
+				profit=arr[i]-min;
+			}
+		}
+		System.out.println(profit);
+	}
 }
