61803f2ca090f2273f50c8f6fd69a9697a19a9d6
piyu989 <sab9861@gmail.com>
2024-10-23 14:34:59 +0530

    .

--- src/Leetcode/AddTwoNumber.java
+++ src/Leetcode/AddTwoNumber.java
@@ -1,7 +1,5 @@
 package Leetcode;
 
-import Leetcode.ReverseLinkedList.Node;
-
 public class AddTwoNumber {
 	static void print(Node head) {
 		Node temp=head;
@@ -20,7 +18,30 @@ public class AddTwoNumber {
 			this.next=null;
 		}
 	}
+	
+	static Node ans(Node l1,Node l2) {
+		int rem=0;
+		Node ans=new Node(-1);
+		Node temp=ans;
+		
+		while(l1!=null||l2!=null|rem!=0) {
+			int sum=rem;
+			if(l1!=null) {
+				sum+=l1.data;
+				l1=l1.next;
+			}
+			if(l2!=null) {
+				sum+=l2.data;
+				l2=l2.next;
+			}
+			rem=sum%10;
+			sum=sum/10;
+			temp.next=new Node(sum);
+			temp=temp.next;
+		}
+		return ans.next;
+	}
 	public static void main(String args[]) {
-		System.out.println("sita ram");
+
 	}
 }
